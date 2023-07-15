0eb19722ca65f152f342cafd2e7748a12f90a6ca
Jose Luis <jchongac@fiec.espol.edu.ec>
2023-07-14 21:20:48 -0500

    empezar proyecto

---
+++ ProyectoPOO/src/main/java/ec/edu/espol/proyectopoo/ProyectoPOO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espol.proyectopoo;
+
+/**
+ *
+ * @author josel
+ */
+public class ProyectoPOO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
