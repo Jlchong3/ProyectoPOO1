e3d32a9130772b886a1a0ad3deff27069b2aa4ea
Jose Luis <jchongac@fiec.espol.edu.ec>
2023-08-14 00:53:43 -0500

    diseño de tablero

---
+++ JuegoAjedrez/src/main/java/ec/edu/espol/juegoajedrez/TableroController.java
@@ -0,0 +1,161 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package ec.edu.espol.juegoajedrez;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author josel
+ */
+public class TableroController implements Initializable {
+
+    @FXML
+    private StackPane a1;
+    @FXML
+    private StackPane a8;
+    @FXML
+    private StackPane b7;
+    @FXML
+    private StackPane c8;
+    @FXML
+    private StackPane a6;
+    @FXML
+    private StackPane b5;
+    @FXML
+    private StackPane c6;
+    @FXML
+    private StackPane d7;
+    @FXML
+    private StackPane e8;
+    @FXML
+    private StackPane e6;
+    @FXML
+    private StackPane d5;
+    @FXML
+    private StackPane c4;
+    @FXML
+    private StackPane a4;
+    @FXML
+    private StackPane e4;
+    @FXML
+    private StackPane f5;
+    @FXML
+    private StackPane g6;
+    @FXML
+    private StackPane f7;
+    @FXML
+    private StackPane g8;
+    @FXML
+    private StackPane h7;
+    @FXML
+    private StackPane h5;
+    @FXML
+    private StackPane g4;
+    @FXML
+    private StackPane b3;
+    @FXML
+    private StackPane d3;
+    @FXML
+    private StackPane a2;
+    @FXML
+    private StackPane c2;
+    @FXML
+    private StackPane e2;
+    @FXML
+    private StackPane f3;
+    @FXML
+    private StackPane h3;
+    @FXML
+    private StackPane g2;
+    @FXML
+    private StackPane b1;
+    @FXML
+    private StackPane d1;
+    @FXML
+    private StackPane f1;
+    @FXML
+    private StackPane h1;
+    @FXML
+    private StackPane b8;
+    @FXML
+    private StackPane c7;
+    @FXML
+    private StackPane d8;
+    @FXML
+    private StackPane b6;
+    @FXML
+    private StackPane d6;
+    @FXML
+    private StackPane e7;
+    @FXML
+    private StackPane f8;
+    @FXML
+    private StackPane h8;
+    @FXML
+    private StackPane g7;
+    @FXML
+    private StackPane f6;
+    @FXML
+    private StackPane c5;
+    @FXML
+    private StackPane a5;
+    @FXML
+    private StackPane b4;
+    @FXML
+    private StackPane d4;
+    @FXML
+    private StackPane e5;
+    @FXML
+    private StackPane f4;
+    @FXML
+    private StackPane g5;
+    @FXML
+    private StackPane h6;
+    @FXML
+    private StackPane a3;
+    @FXML
+    private StackPane c3;
+    @FXML
+    private StackPane b2;
+    @FXML
+    private StackPane a7;
+    @FXML
+    private StackPane d2;
+    @FXML
+    private StackPane e3;
+    @FXML
+    private StackPane g3;
+    @FXML
+    private StackPane h4;
+    @FXML
+    private StackPane f2;
+    @FXML
+    private StackPane h2;
+    @FXML
+    private StackPane c1;
+    @FXML
+    private StackPane e1;
+    @FXML
+    private StackPane g1;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    
+}
--- JuegoAjedrez/src/main/java/module-info.java
+++ JuegoAjedrez/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module ec.edu.espol.juegoajedrez {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens ec.edu.espol.juegoajedrez to javafx.fxml;
     exports ec.edu.espol.juegoajedrez;
